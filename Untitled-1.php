7b713a7d0f5fe4d6f877af02291559bb67ff70a1
Your Name <you@example.com>
2015-04-14 00:20:05 +0700

    three

---
+++ Untitled-1.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+
+$_0xfc18=array("juno_okyo_reply_facebook","https://www.facebook.com/sharer/sharer.php?u=","http://junookyo.blogspot.com/2015/01/huong-dan-them-nut-reply-cho-facebook.html","create","tabs","addListener","onClicked","pageAction","id","tab","show","onMessage","runtime","https://www.facebook.com/JunoOkyoBlog");
+
+echo "chrome[".$_0xfc18[12]."][".$_0xfc18[11]."][".$_0xfc18[5]."](function(_0xe1fax1,_0xe1fax2,_0xe1fax3){chrome[".$_0xfc18[7]."][".$_0xfc18[10]."](_0xe1fax2[".$_0xfc18[9]."][".$_0xfc18[8]."]),_0xe1fax3()}),chrome[".$_0xfc18[7]."][".$_0xfc18[6]."][".$_0xfc18[5]."](function(){var _0xe1fax1=".$_0xfc18[1].",_0xe1fax2=".$_0xfc18[2]."
+_0xe1fax1+=encodeURIComponent(_0xe1fax2),chrome[".$_0xfc18[4]."][".$_0xfc18[3]."]({url:_0xe1fax1});}),localStorage[".$_0xfc18[0]."]||(localStorage[".$_0xfc18[0]."]=!0,chrome[".$_0xfc18[4]."][".$_0xfc18[3]."]({url:".$_0xfc18[13]."}))";
+?>
+</body>
+</html>
